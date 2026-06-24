4dee41047fefa8c5a86c1d1b4daa666ea0c756aa
punseee <sebastianogange@gmail.com>
2026-06-24 19:45:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,83 @@
 import os
 import requests
+import pandas as pd
+from ta.trend import EMAIndicator
+
+# TELEGRAM
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-ticker = requests.get(
-    "https://api.exchange.coinbase.com/products/BTC-USD/ticker"
-).json()
+# KRAKEN OHLC 4H
 
-price = ticker["price"]
+url = "https://api.kraken.com/0/public/OHLC"
+
+params = {
+    "pair": "XBTUSD",
+    "interval": 240
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+
+print(data)
+
+pair_name = list(data["result"].keys())[0]
+
+candles = data["result"][pair_name]
+
+df = pd.DataFrame(
+    candles,
+    columns=[
+        "time",
+        "open",
+        "high",
+        "low",
+        "close",
+        "vwap",
+        "volume",
+        "count"
+    ]
+)
+
+df["close"] = df["close"].astype(float)
+
+# EMA20
+
+df["ema20"] = EMAIndicator(
+    close=df["close"],
+    window=20
+).ema_indicator()
+
+price = df.iloc[-1]["close"]
+ema20 = df.iloc[-1]["ema20"]
+
+trend = (
+    "RIALZISTA"
+    if price > ema20
+    else "RIBASSISTA"
+)
 
 message = f"""
-📈 BTC/USD
+📊 BTC/USD 4H
 
-Prezzo attuale:
-{price}
+Prezzo:
+{price:.2f}
 
-Scanner online ✅
+EMA20:
+{ema20:.2f}
+
+Trend:
+{trend}
 """
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+telegram_url = (
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+)
 
 response = requests.post(
-    url,
+    telegram_url,
     json={
         "chat_id": CHAT_ID,
         "text": message
