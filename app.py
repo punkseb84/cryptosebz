6a334fe25470dd23f913352e5ef37fc3cc0bbcfd
punseee <sebastianogange@gmail.com>
2026-06-24 22:07:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,32 @@ COINS = {
     "BTC": "XBTUSD",
     "ETH": "ETHUSD",
     "SOL": "SOLUSD",
+    "XRP": "XRPUSD",
+    "ADA": "ADAUSD",
+    "DOGE": "DOGEUSD",
     "LINK": "LINKUSD",
-    "AVAX": "AVAXUSD"
+    "AVAX": "AVAXUSD",
+    "DOT": "DOTUSD",
+    "LTC": "LTCUSD",
+    "ATOM": "ATOMUSD",
+    "UNI": "UNIUSD",
+    "AAVE": "AAVEUSD",
+    "FIL": "FILUSD",
+    "ALGO": "ALGOUSD",
+    "ICP": "ICPUSD",
+    "APT": "APTUSD",
+    "ARB": "ARBUSD",
+    "OP": "OPUSD",
+    "NEAR": "NEARUSD",
+    "INJ": "INJUSD",
+    "SUI": "SUIUSD",
+    "SEI": "SEIUSD",
+    "TIA": "TIAUSD",
+    "JUP": "JUPUSD",
+    "SHIB": "SHIBUSD",
+    "PEPE": "PEPEUSD",
+    "ETC": "ETCUSD",
+    "BCH": "BCHUSD"
 }
 
 watchlist = []
