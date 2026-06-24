5cfe41f2006dc9c2d7c09991d9051b73f87bab00
punseee <sebastianogange@gmail.com>
2026-06-24 19:24:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import requests
 url = "https://api.exchange.coinbase.com/products/BTC-USD/candles"
 
 params = {
-    "granularity": 14400
+    "granularity": 21600
 }
 
 response = requests.get(url, params=params)
 
 print("STATUS:", response.status_code)
-print("RISPOSTA:")
-print(response.json())
+
+data = response.json()
+
+print(type(data))
+print(data[:3] if isinstance(data, list) else data)
